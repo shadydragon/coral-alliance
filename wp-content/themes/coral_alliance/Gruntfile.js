ff78adf39d86376c8047dd681c8838dada8257cd
Nicholas Powell <nicholas@punchbuggy.com.au>
2016-11-27 13:31:51 +1100

    Move the build source files into the theme

--- wp-content/themes/coral_alliance/Gruntfile.js
+++ wp-content/themes/coral_alliance/Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = grunt => {
 			},
 			dist: {
 				files: {
-					'../wp-content/themes/coral_alliance/css/site.min.css': 'scss/base.scss'
+					'./css/site.min.css': 'scss/base.scss'
 				}
 			}
 		},
@@ -54,7 +54,7 @@ module.exports = grunt => {
 			},
 			dist: {
 				files: {
-					'../wp-content/themes/coral_alliance/css/site.min.css': '../wp-content/themes/coral_alliance/css/site.min.css'
+					'./css/site.min.css': './css/site.min.css'
 				}
 			}
 		},
@@ -71,14 +71,14 @@ module.exports = grunt => {
                     ]
 				},
 				files: {
-				   "../wp-content/themes/coral_alliance/js/site.min.js": ["js/site.js"]
+				   "./js/site.min.js": ["js/site.js"]
 				}
 			}
 		},
 		uglify: {
 			dist: {
 				files: {
-					'../wp-content/themes/coral_alliance/js/site.min.js': ['../wp-content/themes/coral_alliance/js/site.min.js']
+					'./js/site.min.js': ['./js/site.min.js']
 				}
 			}
 		}
