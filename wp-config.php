b16857d47bc54aa31e8e9605f37e5acf3039a3b5
Nicholas Powell <nicholas@punchbuggy.com.au>
2016-11-27 12:45:53 +1100

    Removes jetpack, adds contact form 7 and contact form DB and changes configuration to work with laragon

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'shadydr1_wptest');
+define('DB_NAME', 'test');
 
 /** MySQL database username */
-define('DB_USER', 'shadydr1_wptest');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', '.ha^~8}U[IMk');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
--- wp-content/themes/coral_alliance/index.php
+++ wp-content/themes/coral_alliance/index.php
@@ -1,3 +1,4 @@
 <?php get_header(); ?>
 	<h1>Home</h1>
+	<?php echo do_shortcode('[cfdb-json form="Contact form 1" role="Anyone" var="petitionFeed" format="arraynoheader"]'); ?>
 <?php get_footer(); ?>
