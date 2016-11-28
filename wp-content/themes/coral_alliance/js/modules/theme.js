9cacb2c39a7249c2da357f317950ffb7baa2a4d6
Nicholas Powell <nicholas@punchbuggy.com.au>
2016-11-28 13:50:41 +1100

    Update the js so that forcing an entries refresh doesn't add a new setInterval

--- wp-content/themes/coral_alliance/js/modules/theme.js
+++ wp-content/themes/coral_alliance/js/modules/theme.js
@@ -21,7 +21,10 @@
 			this.resizeFunctions();
 
 			// Other functions
-			this.pastEntries();
+			this.refreshEntries();
+
+			// Refresh entries every 10 minutes
+			window.setInterval(this.refreshEntries, 600000);
 
 			// run the resize function when the window is resized
 			$(window).resize(this.resizeFunctions);
@@ -43,39 +46,33 @@
 		resizeFunctions: function() {
 			$('.page-template-template-home').find('.hero, .cta').css('min-height', $(window).innerHeight());
 		},
-		pastEntries: function() {
-			// Update the entries and then refresh every 10 minutes.
-			getEntries();
-			window.setInterval(getEntries, 600000);
+		refreshEntries: function() {
+			// Update the entries
+			var arr = window.location.href.split('/'),
+			count = 0,
+			container = $('#past-entries'),
+			compliments = ['became a reef legend', 'swept in and saved the reef', 'joined the reef defenders'];
 
-			function getEntries(){
-				var arr = window.location.href.split('/'),
-				count = 0,
-				container = $('#past-entries'),
-				compliments = ['became a reef legend', 'swept in and saved the reef', 'joined the reef defenders'];
+			$.getJSON(arr[0] + "//" + arr[2] + '/wp-admin/admin-ajax.php?action=cfdb-export&form=Petition&limit=6&enc=JSON&format=map', function(data) {
 
-				$.getJSON(arr[0] + "//" + arr[2] + '/wp-admin/admin-ajax.php?action=cfdb-export&form=Petition&limit=6&enc=JSON&format=map', function(data) {
-
-					// Clean up the old entries
-					container.find('.group').fadeOut(300, function() {
-						$(this).remove();
-						container.append('<div class="group hidden"></div>').find('.group').hide();
-						// Add the new entries
-						$.each(data, function(key, entry) {
-							container.find('.group').append('<div class="item"><div class="time">' + moment(entry.Submitted, "YYYY-MM-DD HH:mm:ss Z").fromNow() + '</div><div class="content">' + entry['full-name'].split(' ')[0] + ' ' + compliments[count] + '</div></div>');
-							if (count >= compliments.length - 1) {
-								count = 0;
-							} else {
-								count++;
-							}
-						});
-						// Show the hidden items
-						container.find('.group.hidden').removeClass('hidden').fadeIn(300);
+				// Clean up the old entries
+				container.find('.group').fadeOut(300, function() {
+					$(this).remove();
+					container.append('<div class="group hidden"></div>').find('.group').hide();
+					// Add the new entries
+					$.each(data, function(key, entry) {
+						container.find('.group').append('<div class="item"><div class="time">' + moment(entry.Submitted, "YYYY-MM-DD HH:mm:ss Z").fromNow() + '</div><div class="content">' + entry['full-name'].split(' ')[0] + ' ' + compliments[count] + '</div></div>');
+						if (count >= compliments.length - 1) {
+							count = 0;
+						} else {
+							count++;
+						}
 					});
-
+					// Show the hidden items
+					container.find('.group.hidden').removeClass('hidden').fadeIn(300);
 				});
 
-			}
+			});
 
 		}
 	};
